e247104814660943177d78a036da8091c15a7400
ar diman <ardim.1775@gmail.com>
2024-02-26 13:56:36 +0800

    validation with __init__

--- 2-Constructor __init__/main.py
+++ 2-Constructor __init__/main.py
@@ -1,32 +1,20 @@
 class Item:
 
-    def __init__(self, nama, harga, kuantitas):
-        # $ini gunanya supaya bisa passing attribute
+    def __init__(self, nama: str, harga: float, kuantitas=0):
+        # validation to recieve argument that we want
+        assert harga >= 0, f"harga: {harga}, harus lebih dari atau sama dengan 0"
+        assert kuantitas >=0, f"kuantitas: {kuantitas}, harus lebih dari atau sama dengan 0"
+
+        # asign to self project
         self.nama = nama
         self.harga = harga
         self.kuantitas = kuantitas
 
     def hitung_harga(self):
-        # kan kita sudah punya akses ke self atau object itu sendiri
-        # maka kita tidak perlu lagi parameter tambahan
         return self.harga * self.kuantitas
 
-# inilah gunanya __init__
-item1 = Item("Smart Phone", 100, 5)
+
+item1 = Item("Smart Phone", 100, -2)
 item2 = Item("Laptop", 1000, 3)
 
-# $kayak gini
-print(item1.nama)
-print(item2.nama)
-print(item1.harga)
-print(item2.harga)
-print(item1.kuantitas)
-print(item2.kuantitas)
-
-# the problem is we hard code the atrribute
-# what if we can use it dinamicly?
-# datanglah yang namanya contructor init
-# item1.nama = "Smart Phone"
-# item1.harga = 100
-# item1.kuantitas = 5
 
