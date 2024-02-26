f632ecc4c1ab5059d483875a0a20d7bbd55e4c98
ar diman <ardim.1775@gmail.com>
2024-02-26 14:38:22 +0800

    class attribute with __repr__

--- 3-Class Attribute/main.py
+++ 3-Class Attribute/main.py
@@ -4,6 +4,7 @@ class Item:
     yang mana sangat bagus untuk membuat sesuatu yang akan dipake semua instance
     di dalam kelas"""
     pay_rate = 0.8 # discount 20%
+    all = []
     def __init__(self, nama: str, harga: float, kuantitas=0):
         # validation to recieve argument that we want
         assert harga >= 0, f"harga: {harga}, harus lebih dari atau sama dengan 0"
@@ -14,6 +15,9 @@ class Item:
         self.harga = harga
         self.kuantitas = kuantitas
 
+        # untuk mengambil semua instance attribute
+        Item.all.append(self)
+
     def hitung_harga(self):
         return self.harga * self.kuantitas
     
@@ -21,21 +25,20 @@ class Item:
         # akses class attribute menggunakan self
         self.harga = self.harga * self.pay_rate
 
+    # __repr__ untuk menampilkan apa ketika class Item diprint
+    def __repr__(self):
+        return f"Item('{self.nama}', {self.harga}, {self.kuantitas})"
 
-item1 = Item("Smart Phone", 100, 1)
+
+item1 = Item("Phone", 100, 1)
 item2 = Item("Laptop", 1000, 3)
-item3 = Item("Smart Watch", 100, 2)
+item3 = Item("Cable", 10, 5)
+item4 = Item("Mouse", 50, 5)
+item5 = Item("Keyboard", 75, 5)
 
-item1.apply_discount()
-print(item1.harga)
+print(Item.all)
 
-# bisa diubah juga kayak instance attribute
-# untuk spesifik gunakan item2 saja instead of Item
-item2.pay_rate = 0.7
-item2.apply_discount()
-print(item2.harga)
-
-item3.apply_discount()
-print(item3.harga)
-
+# print spesifik
+# for instance in Item.all:
+#     print(instance.nama)
 
